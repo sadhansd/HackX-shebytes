6d45099c1fbb1e65e83e1183f310b52065f76591
sadhansd <sadhansd02@gmail.com>
2021-07-31 10:56:03 +0530

    commit 2

---
+++ Shebytes-main/script.js
@@ -0,0 +1,74 @@
+
+const voice = document.querySelector(".voice");
+const voice2text = document.querySelector(".voice2text");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recorder = new SpeechRecognition();
+
+function addHumanText(text) {
+  const chatContainer = document.createElement("div");
+  chatContainer.classList.add("chat-container");
+  const chatBox = document.createElement("p");
+  chatBox.classList.add("voice2text");
+  const chatText = document.createTextNode(text);
+  chatBox.appendChild(chatText);
+  chatContainer.appendChild(chatBox);
+  return chatContainer;
+}
+
+function addBotText(text) {
+  const chatContainer1 = document.createElement("div");
+  chatContainer1.classList.add("chat-container");
+  chatContainer1.classList.add("darker");
+  const chatBox1 = document.createElement("p");
+  chatBox1.classList.add("voice2text");
+  const chatText1 = document.createTextNode(text);
+  chatBox1.appendChild(chatText1);
+  chatContainer1.appendChild(chatBox1);
+  return chatContainer1;
+}
+
+function botVoice(message) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = "Sorry, I did not understand that.";
+
+    if (message.includes('how do I know')) {
+      speech.text = "You’ll know when you know.  people say that from the time they were very young they just “felt different” or “just knew” they weren’t like their friends.";
+    }
+
+    if (message.includes('come out')) {
+      speech.text = "Only if you want to, and only when you’re ready and feel safe to do so. Don’t come out just because someone else thinks you should";
+    }
+
+    if (message.includes('find other lgbtq friends')) {
+      speech.text = "Find friends through Facebook, Twitter, and Tumblr. volunteering with LGBT organizations will help you found comfort and confidence";
+    }
+
+    if (message.includes('harassed')) {
+      speech.text = "contact your local police or tell a trusted adult. Visit pflag.org/claimyourrights for more information"
+    }
+    
+
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+    var element = document.getElementById("container");
+    element.appendChild(addBotText(speech.text));
+}
+
+recorder.onstart = () => {
+  console.log('Voice activated');
+};
+
+recorder.onresult = (event) => {
+  const resultIndex = event.resultIndex;
+  const transcript = event.results[resultIndex][0].transcript;
+  var element = document.getElementById("container");
+  element.appendChild(addHumanText(transcript));
+  botVoice(transcript);
+};
+
+voice.addEventListener('click', () =>{
+  recorder.start();
+});
